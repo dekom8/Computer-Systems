b1bbe940bbb5f482fc3056c76278c73e45890184
dekom8 <38499476+dekom8@users.noreply.github.com>
2019-04-14 23:26:49 +0300

    Lab3

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+a="COMMON-AVX512 MIC-AVX512 CORE-AVX512 CORE-AVX2 CORE-AVX-I AVX SSE4.2 SSE4.1 SSSE3 SSE3 SSE2"
+#a=`cat /proc/cpuinfo | grep flags | uniq | cut -d: -f2`
+for i in $a
+do
+	icc -std=c99 -qopt-report=2 -qopt-report-phase=vec -D NOALIAS -D ALIGNED -ipo code.cpp -o code6 -x$i
+	if [ $? -eq 0 ]
+	then
+			echo "-------------------------------------------------">&2
+			echo "Works for $i">&2
+			time ./code6
+			echo "-------------------------------------------------">&2
+	fi
+done
